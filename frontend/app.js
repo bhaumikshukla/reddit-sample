7481027880a3db1f09d361d52f848c614b05aed6
Bhaumik Shukla <bhaumik.shukla@einfochips.com>
2017-08-04 23:41:59 +0530

    ADDED: Angularjs: initial UI files, webserver config files, and minimal css

---
+++ frontend/app.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    // routes are provided here
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' }); // if nothing workout then
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+    function run($rootScope, $location, $cookies, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookies.getObject('globals') || {};
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var loggedIn = $rootScope.globals.currentUser;
+            if (!loggedIn) {
+                $location.path('/login');
+            }
+        });
+    }
+
+})();
