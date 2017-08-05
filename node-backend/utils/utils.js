8375320495ab30197355c42a789f098e3d226f67
Bhaumik Shukla <bhaumik.shukla@einfochips.com>
2017-08-05 17:19:16 +0530

    ADDED: in backend, some more default data, helps in tests

--- node-backend/utils/utils.js
+++ node-backend/utils/utils.js
@@ -6,7 +6,7 @@ Provides setter getter methods for topics, just to make these utilities separate
 var dic = {
   "I did not agree to this bed switch.-Dog" : {
     "votes":2,
-    "user": "bhaumik"
+    "user": "bot"
   },
   "Government’s claim that only wrong-doers need privacy shocks the court" : {
     "votes":23,
@@ -15,6 +15,34 @@ var dic = {
   "Hawaii from the sky. My best friend's Instagram is full of these as he is a pilot that sees this stuff almost daily. @kevin_mcfall [1039x1350]" : {
     "votes":34,
     "user": "x"
+  },
+  "Horrified McDonald's customers watch naked man inject drugs into his testicles" : {
+    "votes":52,
+    "user": "bot"
+  },
+  "Please stop visiting me Flatulence Angel!" : {
+    "votes":28,
+    "user": "bot"
+  },
+  "Maharashtra CM Says Mumbai Free Of Open Defecation. That’s Not What We Found - FactChecker" : {
+    "votes":65,
+    "user": "bot"
+  },
+  "Mumbai scientists to protest against cuts in research funding on August 9" : {
+    "votes":56,
+    "user": "bot"
+  },
+  "The Rebels would have you believe those serving the Empire are monsters. We are as human as you" : {
+    "votes":77,
+    "user": "bot"
+  },
+  "Captain America had no idea about Spider-Man's super strength, and this is what he intended to happen!" : {
+    "votes":89,
+    "user": "bot"
+  },
+  "Apparently scorpion mouths are made from nightmares" : {
+    "votes":76,
+    "user": "bot"
   }
 };
 
