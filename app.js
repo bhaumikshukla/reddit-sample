ddfa00a9a9e4897f7d9936e6913dc4835f204596
Bhaumik Shukla <bhaumik.shukla@einfochips.com>
2017-08-06 11:48:39 +0530

    MODIFIED: Structure to make one app only. placed app.js package.js in front dir, applied relevant changes for one app, MODIFIED: html path in view

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+This is the main module. Responsible to run the application and gather or initiate the required resources.
+*/
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var frontend = path.join(__dirname, 'frontend');
+
+
+app.use(express.static(frontend));
+
+// provided routes in routes.js, all the APIs definitions are there
+var routes = require("./node-backend/routes/routes.js")(app);
+
+// starts the application and starting listening on mentioned port
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
